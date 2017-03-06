f510c95153c65548ed2f3500e2eb05844a038831
relucent <relucent@163.com>
2017-03-06 15:05:15 +0800

    #

--- src/main/java/yyl/mvc/core/util/page/PageUtil.java
+++ src/main/java/yyl/mvc/core/util/page/PageUtil.java
@@ -1,9 +1,5 @@
 package yyl.mvc.core.util.page;
 
-import java.util.List;
-
-import yyl.mvc.core.util.page.impl.SimplePage;
-
 /**
  * 分页查询帮助类，提供一些分页查询需要的计算方法.
  * @author YYL
@@ -35,17 +31,6 @@ public class PageUtil {
 		} else {
 			return (int) ((count - 1) / pageSize) + 1;
 		}
-	}
-
-	/**
-	 * 创建一个Page对象
-	 * @param start 记录开始索引号
-	 * @param limit 页面最大记录数
-	 * @param records 当前页数据
-	 * @param total 总记录数
-	 */
-	public static <T> Page<T> newSimplePage(int start, int limit, List<T> records, int total) {
-		return new SimplePage<T>(start, limit, records, total);
 	}
 
 	//	public static Page<T> cast(Page<T> page, RecordConverter converter) {
