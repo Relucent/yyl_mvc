93ce607b4ab8458f8492a0712a814d0118544d57
Administrator <Administrator@THINKPAD-PC>
2018-03-19 19:14:34 +0800

    #

--- src/main/java/yyl/mvc/core/plug/spring/jdbc/JdbcDaoHelper.java
+++ src/main/java/yyl/mvc/core/plug/spring/jdbc/JdbcDaoHelper.java
@@ -1,19 +1,27 @@
 package yyl.mvc.core.plug.spring.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import com.google.common.collect.Lists;
 
 import yyl.mvc.core.plug.jdbc.dialect.Dialect;
 import yyl.mvc.core.plug.jdbc.dialect.DialectConfigurer;
 import yyl.mvc.core.util.page.Page;
+import yyl.mvc.core.util.page.Pagination;
 import yyl.mvc.core.util.page.impl.SimplePage;
 
 /**
- * 基于JDBC的数据访问层支持工具类.
+ * 基于JDBC的数据访问层支持工具类.<br>
+ * @author _yyl
  */
 public class JdbcDaoHelper {
 
@@ -27,6 +35,41 @@ public class JdbcDaoHelper {
 	// select '1' from dual where exists( select '1' from {table} )
 	public static boolean exists(String sql, Object[] args, JdbcTemplate jdbcTemplate) {
 		return jdbcTemplate.queryForObject("select count(1) from dual where exists( " + sql + " )", args, Long.class).longValue() > 0L;
+	}
+
+	/**
+	 * 插入数据并返回主键
+	 * @param sql SQL语句
+	 * @param args 参数
+	 * @param jdbcTemplate JDBC模板
+	 * @return 主键
+	 */
+	public static Long insertAndGeneratedKey(String sql, Object[] args, JdbcTemplate jdbcTemplate) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+				int i = 0;
+				for (Object arg : args) {
+					ps.setObject(++i, arg);
+				}
+				return ps;
+			}
+		}, keyHolder);
+		return keyHolder.getKey().longValue();
+	}
+
+	/**
+	 * 查询单个记录
+	 * @param sql 查询单个数据
+	 * @param args 查询参数
+	 * @param rowMapper 行映射
+	 * @param jdbcTemplate JDBC模板
+	 * @return 分页查询結果
+	 */
+	public static <T> T queryOne(String sql, Object[] args, RowMapper<T> rowMapper, JdbcTemplate jdbcTemplate) {
+		return getSingleResult(jdbcTemplate.query(sql, args, rowMapper));
 	}
 
 	/**
@@ -58,22 +101,42 @@ public class JdbcDaoHelper {
 			Dialect dialect) {
 		int count = jdbcTemplate.queryForObject(dialect.getCountSql(sql), args, Long.class).intValue();
 		if (count == 0) {
-			return new SimplePage<T>(start, limit, Lists.<T> newArrayList(), 0);
+			return new SimplePage<T>(start, limit, Lists.<T>newArrayList(), 0);
 		}
 		List<T> records = jdbcTemplate.query(dialect.getLimitSql(sql, start, limit), args, rowMapper);
 		return new SimplePage<T>(start, limit, records, count);
 	}
 
 	/**
-	 * 查询单个记录
-	 * @param sql 查询单个数据
+	 * 分页查询
+	 * @param sql 查询语句
 	 * @param args 查询参数
+	 * @param start 第一条记录索引
+	 * @param limit 每页显示记录数
 	 * @param rowMapper 行映射
 	 * @param jdbcTemplate JDBC模板
+	 * @param dialect 方言
 	 * @return 分页查询結果
 	 */
-	public static <T> T queryOne(String sql, Object[] args, RowMapper<T> rowMapper, JdbcTemplate jdbcTemplate) {
-		return getSingleResult(jdbcTemplate.query(sql, args, rowMapper));
+	public static <T> Page<T> pagedQuery(CharSequence sql, List<Object> args, int start, int limit, RowMapper<T> rowMapper, JdbcTemplate jdbcTemplate,
+			Dialect dialect) {
+		return pagedQuery(sql.toString(), args.toArray(), start, limit, rowMapper, jdbcTemplate, dialect);
+	}
+
+	/**
+	 * 分页查询
+	 * @param sql 查询语句
+	 * @param args 查询参数
+	 * @param pagination 分页限制
+	 * @param limit 每页显示记录数
+	 * @param rowMapper 行映射
+	 * @param jdbcTemplate JDBC模板
+	 * @param dialect 方言
+	 * @return 分页查询結果
+	 */
+	public static <T> Page<T> pagedQuery(CharSequence sql, List<Object> args, Pagination pagination, RowMapper<T> rowMapper,
+			JdbcTemplate jdbcTemplate, Dialect dialect) {
+		return pagedQuery(sql.toString(), args.toArray(), pagination.getStart(), pagination.getLimit(), rowMapper, jdbcTemplate, dialect);
 	}
 
 	// ==============================Tools_Methods====================================
