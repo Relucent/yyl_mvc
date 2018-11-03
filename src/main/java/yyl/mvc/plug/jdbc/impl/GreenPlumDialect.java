4b0b120ee34452c645c2118bc87da01c02b2e034
yaoyilang <yaoyilang@byitgroup.com>
2018-11-03 20:16:22 +0800

    #

---
+++ src/main/java/yyl/mvc/plug/jdbc/impl/GreenPlumDialect.java
@@ -0,0 +1,26 @@
+package yyl.mvc.plug.jdbc.impl;
+
+import yyl.mvc.plug.jdbc.Dialect;
+
+/**
+ * JDBC查询方言GreenPlum实现，主要用于提供分页查询<br>
+ */
+public class GreenPlumDialect implements Dialect {
+
+    public static final GreenPlumDialect INSTANCE = new GreenPlumDialect();
+
+    @Override
+    public String getCountSql(String sql) {
+        return "select count(*) as COUNT___y from (" + sql + ") T___T";
+    }
+
+    @Override
+    public String getLimitSql(String sql, int offset, int limit) {
+        return sql + " limit " + limit + " offset " + offset;
+    }
+
+    @Override
+    public String testQuery() {
+        return "select version()";
+    }
+}
