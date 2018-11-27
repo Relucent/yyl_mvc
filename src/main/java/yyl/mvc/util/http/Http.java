92266238bebd31b0c3c323a8e1ddba2bd1b0ae67
yaoyilang <yaoyilang@byitgroup.com>
2018-11-27 22:54:23 +0800

    OK

--- src/main/java/yyl/mvc/util/http/Http.java
+++ src/main/java/yyl/mvc/util/http/Http.java
@@ -2,13 +2,16 @@ package yyl.mvc.util.http;
 
 import java.net.URL;
 
+/**
+ * Http Tool.
+ */
 public class Http {
 
-	public static Connection connect(String url) {
-		return HttpConnection.connect(url);
-	}
+    public static Connection connect(String url) {
+        return Connection.connect(url);
+    }
 
-	public static Connection connect(URL url) {
-		return HttpConnection.connect(url);
-	}
+    public static Connection connect(URL url) {
+        return Connection.connect(url);
+    }
 }
--- src/main/java/yyl/mvc/util/http/v1/HttpUtil.java
+++ src/main/java/yyl/mvc/util/http/v1/HttpUtil.java
@@ -9,179 +9,142 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
-import java.util.List;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
 
 /**
  * HTTP工具类.
- * @author YaoYiLang
- * @version 2014-12-06 09:30
+ * @author YYL
+ * @version 2005-12-20
  */
 public class HttpUtil {
 
     // =================================Fields=================================================
     // =================================Constants==============================================
     public static final String GET = "GET";
-    public static final String PUT = "PUT";
-    public static final String HEAD = "HEAD";
     public static final String POST = "POST";
-    public static final String PATCH = "PATCH";
-    public static final String DELETE = "DELETE";
-
     public static final String UTF_8 = "UTF-8";
-    public static final Charset UTF_8_CHARSET = Charset.forName("UTF-8");// UTF-8 字符集
+    public static final int CONNECT_TIMEOUT = 10000;// 连接超时(单位毫秒)
+    public static final int READ_TIMEOUT = 30000;// 读取超时(单位毫秒)
 
-    public static final int DEFAULT_CONNECT_TIMEOUT = 15000;// 连接超时(单位毫秒)
-    public static final int DEFAULT_READ_TIMEOUT = 30000;// 读取超时(单位毫秒)
+    public static final SSLSocketFactory TRUST_ANY_SSL_SOCKET_FACTORY;
+    public static final HostnameVerifier TRUST_ANY_HOSTNAME_VERIFIER;
 
     // =================================Methods================================================
     /**
      * 发送GET请求
-     * @param url
+     * @param url URL地址
      * @return 请求的结果
      */
-    public static Response get(String url) {
+    public static String get(String url) {
         return get(url, null, null);
     }
 
     /**
      * 发送GET请求
-     * @param url
+     * @param url URL地址
      * @param queryParams URL参数
      * @return 请求的结果
      */
-    public static Response get(String url, Map<String, String> queryParams) {
+    public static String get(String url, Map<String, String> queryParams) {
         return get(url, queryParams, null);
     }
 
     /**
      * 发送GET请求
-     * @param url
+     * @param url URL地址
      * @param queryParams URL参数
      * @param headers HTTP头数据
      * @return 请求的结果
      */
-    public static Response get(String url, Map<String, String> queryParams, Map<String, String> headers) {
+    public static String get(String url, Map<String, String> queryParams, Map<String, String> headers) {
         HttpURLConnection conn = null;
         try {
             conn = getHttpConnection(buildUrlWithQueryString(url, queryParams), GET, headers);
             conn.connect();
-            return getHttpResponse(conn);
+            return getResponse(conn);
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-            closeQuietly(conn);
+            disconnect(conn);
         }
     }
 
     /**
      * 发送POST请求
-     * @param url
-     * @param data 提交数据
+     * @param url URL地址
      * @return 请求的结果
      */
-    public static Response post(String url, byte[] data) {
-        return post(url, null, data, null);
+    public static String post(String url) {
+        return post(url, null, null);
     }
 
     /**
      * 发送POST请求
-     * @param url
-     * @param queryParams URL参数
-     * @param data 提交数据
+     * @param url URL地址
+     * @param queryParams 查询参数
      * @return 请求的结果
      */
-    public static Response post(String url, Map<String, String> queryParams, byte[] data) {
-        return post(url, queryParams, data, null);
+    public static String post(String url, Map<String, String> queryParams) {
+        return post(url, queryParams, null);
     }
 
     /**
      * 发送POST请求
-     * @param url
-     * @param data 提交数据
+     * @param url URL地址
+     * @param queryParams 查询参数
      * @param headers HTTP头数据
      * @return 请求的结果
      */
-    public static Response post(String url, byte[] data, Map<String, String> headers) {
-        return post(url, null, data, headers);
-    }
-
-    /**
-     * 发送POST请求
-     * @param url
-     * @param queryParams URL参数
-     * @param data 提交数据
-     * @param headers HTTP头数据
-     * @return 请求的结果
-     */
-    public static Response post(String url, Map<String, String> queryParams, byte[] data, Map<String, String> headers) {
+    public static String post(String url, Map<String, String> queryParams, Map<String, String> headers) {
         HttpURLConnection conn = null;
         try {
-            conn = getHttpConnection(buildUrlWithQueryString(url, queryParams), POST, headers);
+            conn = getHttpConnection(url, POST, headers);
             conn.connect();
-            OutputStream out = null;
-            try {
-                out = conn.getOutputStream();
-                out.write(data == null ? new byte[0] : data);
-                out.flush();
-            } finally {
-                if (out != null) {
-                    out.close();
-                }
-            }
-            return getHttpResponse(conn);
+            writeAndClose(conn.getOutputStream(), buildWithQueryString(queryParams));
+            return getResponse(conn);
         } catch (Exception e) {
-            e.printStackTrace();
             throw new RuntimeException(e);
         } finally {
-            closeQuietly(conn);
+            disconnect(conn);
         }
     }
 
     /**
      * 获得一个HTTP连接
-     * @param url URL
-     * @param method 请求的方法
-     * @throws IOException
-     */
-    public static HttpURLConnection getHttpConnection(String url, String method) throws IOException {
-        return getHttpConnection(url, method, null);
-    }
-
-    /**
-     * 获得一个HTTP连接
-     * @param url URL
+     * @param url URL地址
      * @param method 请求的方法
      * @param headers HTTP头数据
      */
     public static HttpURLConnection getHttpConnection(String url, String method, Map<String, String> headers)
-            throws IOException {
-        URL _url = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) _url.openConnection();
-        if (conn instanceof HttpsURLConnection) {
-            ((HttpsURLConnection) conn).setSSLSocketFactory(TrustAnyTrustManager.getInsecureSSLSocketFactory());
-            ((HttpsURLConnection) conn).setHostnameVerifier(TrustAnyHostnameVerifier.getInsecureVerifier());
-        }
+            throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
+
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        ignoreTLS(conn);
         conn.setRequestMethod(method);
         conn.setDoOutput(true);
         conn.setDoInput(true);
-        conn.setUseCaches(false);
-        conn.setConnectTimeout(DEFAULT_CONNECT_TIMEOUT);
-        conn.setReadTimeout(DEFAULT_READ_TIMEOUT);
+        conn.setConnectTimeout(CONNECT_TIMEOUT);
+        conn.setReadTimeout(READ_TIMEOUT);
 
-        // if (headers == null || !headers.containsKey("Content-Type")) {
-        // conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;
-        // charset=UTF-8");
-        // }
-        // if (headers == null || !headers.containsKey("User-Agent")) {
-        // conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64)
-        // AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36");
-        // }
-
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        conn.setRequestProperty("User-Agent",
+                "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36");
         if (headers != null && !headers.isEmpty()) {
             for (Entry<String, String> entry : headers.entrySet()) {
                 conn.setRequestProperty(entry.getKey(), entry.getValue());
@@ -191,79 +154,135 @@ public class HttpUtil {
     }
 
     /**
-     * 设置HTTP请求头
+     * 忽略 HTTPS
      * @param conn HTTP连接
-     * @param headers HTTP请求头
      */
-    public static void setRequestHeaders(HttpURLConnection conn, Map<String, List<String>> headers) {
-        for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
-            String name = entry.getKey();
-            List<String> values = entry.getValue();
-            for (String value : values) {
-                conn.setRequestProperty(name, value);
-            }
+    public static void ignoreTLS(HttpURLConnection conn) {
+        if (conn instanceof HttpsURLConnection) {
+            ((HttpsURLConnection) conn).setSSLSocketFactory(TRUST_ANY_SSL_SOCKET_FACTORY);
+            ((HttpsURLConnection) conn).setHostnameVerifier(TRUST_ANY_HOSTNAME_VERIFIER);
         }
     }
 
     /**
-     * 写入请求主体数据
-     * @param conn HTTP连接
-     * @param body 请求主体数据
-     * @throws IOException 发生IO异常时候
+     * 拼装查询字符串
+     * @param queryParams 查询参数
+     * @return 查询字符串
      */
-    public static void setRequestBody(HttpURLConnection conn, byte[] body) throws IOException {
-        OutputStream out = null;
-        try {
-            out = conn.getOutputStream();
-            out.write(body == null ? new byte[0] : body);
-            out.flush();
-        } finally {
-            if (out != null) {
-                out.close();
-            }
-        }
+    public static String buildWithQueryString(Map<String, String> queryParams) {
+        StringBuilder builder = new StringBuilder();
+        buildWithQueryString(queryParams, builder);
+        return builder.toString();
     }
 
     /**
      * 构建带条件的URL字符串
      * @param url URL地址
-     * @param query 查询条件
+     * @param queryParams 查询参数
      */
-    private static String buildUrlWithQueryString(String url, Map<String, String> query) {
-        if (query == null || query.isEmpty()) {
+    public static String buildUrlWithQueryString(String url, Map<String, String> queryParams) {
+        if (queryParams == null || queryParams.isEmpty()) {
             return url;
         }
-        StringBuilder sb = new StringBuilder(url);
-        boolean first;
+        StringBuilder builder = new StringBuilder(url);
         if (url.indexOf("?") == -1) {
-            first = true;
-            sb.append("?");
-        } else {
-            first = false;
+            builder.append("?");
         }
-        for (Entry<String, String> entry : query.entrySet()) {
+        buildWithQueryString(queryParams, builder);
+        return builder.toString();
+    }
+
+    /**
+     * 拼装查询字符串
+     * @param queryParams 查询参数
+     * @param builder 可变的字符序列
+     * @return 查询字符串
+     */
+    private static void buildWithQueryString(Map<String, String> queryParams, StringBuilder builder) {
+        if (queryParams != null && !queryParams.isEmpty()) {
+            return;
+        }
+        boolean first = false;
+        for (Entry<String, String> entry : queryParams.entrySet()) {
             if (first) {
                 first = false;
             } else {
-                sb.append("&");
+                builder.append("&");
             }
-            String key = entry.getKey();
+            String name = entry.getKey();
             String value = entry.getValue();
-            if (value != null && !value.isEmpty()) try {
-                value = URLEncoder.encode(value, UTF_8);
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
+            if (value != null && !value.isEmpty()) {
+                continue;
             }
-            sb.append(key).append("=").append(value);
+            builder.append(encodeUrl(name)).append("=").append(encodeUrl(value));
         }
-        return sb.toString();
+    }
+
+    /**
+     * 读取HTTP响应数据
+     * @param conn HTTP连接
+     * @return HTTP响应数据
+     */
+    public static String getResponse(HttpURLConnection conn) throws IOException {
+        if (conn.getResponseCode() != 200) {
+            throw new IOException("Response Code:" + conn.getResponseCode());
+        }
+        return readAndClose(conn.getInputStream());
+    }
+
+    /**
+     * 写入输出流
+     * @param output 输入流
+     * @param data 写入数据
+     */
+    private static void writeAndClose(OutputStream output, String data) throws IOException {
+        try {
+            output.write(data.getBytes(UTF_8));
+            output.flush();
+        } finally {
+            closeQuietly(output);
+        }
+    }
+
+    /**
+     * 读取输入流
+     * @param input 输入流
+     * @return 输入流的内容
+     */
+    private static String readAndClose(InputStream input) throws IOException {
+        ByteArrayOutputStream output = null;
+        try {
+            output = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4096];
+            int n = 0;
+            while (-1 != (n = input.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+            return new String(output.toByteArray(), UTF_8);
+        } finally {
+            closeQuietly(output);
+            closeQuietly(input);
+        }
+    }
+
+    /**
+     * URL字符转义
+     * @param value 字符串
+     * @return 转义后的字符串
+     */
+    public static String encodeUrl(String value) {
+        try {
+            return URLEncoder.encode(value, UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
      * 关闭连接
      * @param conn HTTP连接
      */
-    public static void closeQuietly(HttpURLConnection conn) {
+    public static void disconnect(HttpURLConnection conn) {
         if (conn != null) {
             try {
                 conn.disconnect();
@@ -272,76 +291,46 @@ public class HttpUtil {
     }
 
     /**
-     * 静默方式关闭连接(忽略异常)
-     * @param closeable 可以关闭的对象
+     * 关闭流
+     * @param stream 数据流
      */
-    private static void closeQuietly(Closeable closeable) {
-        try {
-            if (closeable != null) {
-                closeable.close();
+    private static void closeQuietly(Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                // Ignore
             }
-        } catch (IOException ioe) {}
+        }
     }
 
-    /**
-     * 读取HTTP响应数据
-     * @param conn HTTP连接
-     * @return HTTP响应数据
-     */
-    public static Response getHttpResponse(HttpURLConnection conn) {
-        Response.Builder builder = new Response.Builder();
+    /** 初始化 */
+    static {
+        TRUST_ANY_HOSTNAME_VERIFIER = new TrustAnyHostnameVerifier();
         try {
-            builder.code(conn.getResponseCode());
-            builder.message(conn.getResponseMessage());
-            for (Map.Entry<String, List<String>> entry : conn.getHeaderFields().entrySet()) {
-                String name = entry.getKey();
-                for (String value : entry.getValue()) {
-                    builder.addHeader(name, value);
-                }
-            }
-            InputStream input = null;
-            try {
-                builder.body(toByteArray(input = conn.getInputStream()));
-            } finally {
-                closeQuietly(input);
-            }
+            SSLContext sslContext = SSLContext.getInstance("TLS", "SunJSSE");
+            sslContext.init(null, new TrustManager[] {new TrustAnyTrustManager()}, new java.security.SecureRandom());
+            TRUST_ANY_SSL_SOCKET_FACTORY = sslContext.getSocketFactory();
         } catch (Exception e) {
-            try {
-                builder.code(conn.getResponseCode());
-                builder.message(conn.getResponseMessage());
-                InputStream input = null;
-                try {
-                    builder.body(toByteArray(input = conn.getErrorStream()));
-                } finally {
-                    closeQuietly(input);
-                }
-            } catch (Exception t) {
-                throw new RuntimeException(t);
-            }
+            throw new RuntimeException(e);
         }
-        return builder.build();
     }
 
-    /**
-     * 转换输入流为字节数组
-     * @param input 输入流
-     * @return 输入流的内容(字节数组)
-     * @throws IOException
-     */
-    private static byte[] toByteArray(InputStream input) throws IOException {
-        if (input == null) {
+    // =================================InnerClasses===========================================
+    /** HTTPS域名校验 */
+    private static class TrustAnyHostnameVerifier implements HostnameVerifier {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
+    /** HTTPS证书管理 */
+    private static class TrustAnyTrustManager implements X509TrustManager {
+        public X509Certificate[] getAcceptedIssuers() {
             return null;
-        } else {
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            byte[] buffer = new byte[4096];
-            int n = 0;
-            while (-1 != (n = input.read(buffer))) {
-                output.write(buffer, 0, n);
-            }
-            return output.toByteArray();
         }
-    }
 
-    // header("Proxy-Authorization", "Basic " + Base64.encodeBase64String((proxy.username() + ":" +
-    // proxy.password()).getBytes()));
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {}
+    }
 }
